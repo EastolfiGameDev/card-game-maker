741b61bbc8da10ad5d02de1c2f14a1acf1854055
Eduardo Astolfi <eduardo.astolfi@prima-solutions.com>
2019-06-06 11:50:52 +0200

    into initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'cgm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: "cgm-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'card-game-maker';
+    title = "card-game-maker";
 }
